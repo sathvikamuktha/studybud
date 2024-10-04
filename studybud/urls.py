49fd0f9edf253f28f625dc9637441d0850dba8df
[sathvika muktha] <[sathvikamuktha@gmail.com]>
2024-10-04 12:11:41 -0400

    finals

--- studybud/urls.py
+++ studybud/urls.py
@@ -4,9 +4,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('base.urls')),
+
     path('api/', include('base.api.urls'))
 ]
 
